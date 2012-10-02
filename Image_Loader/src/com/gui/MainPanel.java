16a0e79e3257c004241fb145f34ffceb047d5cba
Yordan Petrov <s0939895@holywood.inf.ed.ac.uk>
2012-10-02 18:08:43 +0100

    Added files

---
+++ Image_Loader/src/com/gui/MainPanel.java
@@ -0,0 +1,69 @@
+package com.gui;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+
+public class MainPanel extends JPanel  {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private BufferedImage image;
+
+
+	public MainPanel() {
+		this.setVisible(true);
+
+		Dimension panelSize = new Dimension(800, 600);
+		this.setSize(panelSize);
+		this.setMinimumSize(panelSize);
+		this.setPreferredSize(panelSize);
+		this.setMaximumSize(panelSize);
+		}
+	
+	/**
+	 * extended constructor - loads image to be labelled
+	 * @param imageName - path to image
+	 * @throws Exception if error loading the image
+	 */
+	public MainPanel( String imageName) throws Exception{
+		this();
+		image = ImageIO.read(new File(imageName));
+		if (image.getWidth() > 800 || image.getHeight() > 600) {
+			int newWidth = image.getWidth() > 800 ? 800 : (image.getWidth() * 600)/image.getHeight();
+			int newHeight = image.getHeight() > 600 ? 600 : (image.getHeight() * 800)/image.getWidth();
+			System.out.println("SCALING TO " + newWidth + "x" + newHeight );
+			Image scaledImage = image.getScaledInstance(newWidth, newHeight, Image.SCALE_FAST);
+			image = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+			image.getGraphics().drawImage(scaledImage, 0, 0, this);
+		}
+	}
+	
+	/**
+	 * Displays the image
+	 */
+	public void ShowImage() {
+		Graphics g = this.getGraphics();
+		
+		if (image != null) {
+			g.drawImage(
+					image, 0, 0, null);
+		}
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		
+		//display iamge
+		ShowImage();
+	}
+}
