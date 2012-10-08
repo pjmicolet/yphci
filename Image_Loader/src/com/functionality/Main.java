7d390318a6005f47b735f0765a96f34c5bdd4d7d
Paul-Jules Micolet <p.micolet@gmail.com>
2012-10-08 09:22:14 +0100

    stashed

--- Image_Loader/src/com/functionality/Main.java
+++ Image_Loader/src/com/functionality/Main.java
@@ -15,7 +15,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		MainFrame mainFrame = new MainFrame("/Users/paul-julesmicolet/Pictures/4chanfile/1348777794239.jpeg");
+		MainFrame mainFrame = new MainFrame("/Users/paul-julesmicolet/Pictures/4chanfile/1349134526073.jpeg");
 		
 		//imageFrame.setContentPane(mainPanel);
 		//toolsFrame.setContentPane(sidePanel);
